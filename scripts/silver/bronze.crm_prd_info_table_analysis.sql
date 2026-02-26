a247e1352d26d53278fd7835baea8c47481e92bc
Ranielo Xavier A. Remo <rxavierremo@outlook.com>
2026-02-27 00:52:23 +0800

    Added bronze.crm_prd_info_table_analysis.sql

--- scripts/silver/bronze.crm_prd_info_table_analysis.sql
+++ scripts/silver/bronze.crm_prd_info_table_analysis.sql
@@ -1,1 +1,23 @@
+--------------------------------------------------------------------------------------------------------------------------
+-- Check for NULLS or Duplicates in the Primary Key
+-- Expectation: No result
+--------------------------------------------------------------------------------------------------------------------------
+SELECT DISTINCT
+  prd_id,
+  COUNT(*)
+FROM bronze.crm_prd_info
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+SELECT
+  prd_id,
+  prd_key,
+  prd_nm,
+  prd_cost,
+  prd_line,
+  prd_start_dt,
+  prd_end_dt
+FROM bronze.crm_prd_info
+
+
+
 
