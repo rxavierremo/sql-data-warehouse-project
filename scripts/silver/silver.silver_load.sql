215b05036371757e535ad07b2bd74cc278857c4c
Ranielo Xavier A. Remo <rxavierremo@outlook.com>
2026-02-26 20:49:08 +0800

    Update silver.silver_load.sql

--- scripts/silver/silver.silver_load.sql
+++ scripts/silver/silver.silver_load.sql
@@ -17,7 +17,8 @@ BEGIN
 		cst_lastname NVARCHAR(50),
 		cst_marital_status NVARCHAR(20),
 		cst_gndr NVARCHAR(10),
-		cst_create_date DATE
+		cst_create_date DATE,
+		dwh_create_date DATETIME2 DEFAULT GETDATE() -- Metadata column that records the current date when this script is executed
 	);
 	PRINT 'Table silver.crm_cust_info has been created successfully';
 END
@@ -36,7 +37,8 @@ BEGIN
 		prd_cost INT,
 		prd_line NVARCHAR(50),
 		prd_start_dt DATE,
-		prd_end_dt DATE
+		prd_end_dt DATE,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 	PRINT 'Table silver.crm_prd_info has been created successfully';
 END
@@ -57,7 +59,8 @@ BEGIN
 		sls_due_dt INT,
 		sls_sales INT,
 		sls_quantity INT,
-		sls_price INT
+		sls_price INT,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 	PRINT 'Table silver.crm_sales_details has been created successfully';
 END
@@ -73,6 +76,7 @@ BEGIN
 		cid NVARCHAR,
 		bdate DATE,
 		gen NVARCHAR(15),
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 	PRINT 'Table silver.erp_cust_az12 has been created successfully';
 END
@@ -87,6 +91,7 @@ BEGIN
 	CREATE TABLE silver.erp_loc_a101 (
 		cid NVARCHAR(50),
 		cntry NVARCHAR(50),
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 	PRINT 'Table silver.erp_loc_a101 has been created successfully';
 END
@@ -103,6 +108,7 @@ BEGIN
 		cat NVARCHAR(50),
 		subcat NVARCHAR(50),
 		maintenance NVARCHAR(10),
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
 	);
 	PRINT 'Table silver.erp_px_cat_g1v2 has been created successfully';
 END
